9460e3007f70b33f4bdd7278cd71b28b425411f2
Combi153 <jcm04@naver.com>
2024-01-08 23:56:12 +0900

    [feat] project setting

---
+++ src/main/java/yeahhh/YeahhhApplication.java
@@ -0,0 +1,13 @@
+package yeahhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeahhhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeahhhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeahhh/YeahhhApplicationTests.java
@@ -0,0 +1,13 @@
+package yeahhh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeahhhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
